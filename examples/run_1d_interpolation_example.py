3533f38c7d24a16855e43e0252e1308234988650
IsaenkovDanil <isaenkov.dn@phystech.edu>
2025-03-23 17:47:27 +0300

    Добавлена обработка изображений

--- examples/run_1d_interpolation_example.py
+++ examples/run_1d_interpolation_example.py
@@ -2,22 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from interpolation_lib.Interpolation_1D_Methods.py import linear_interpolation_1d
 
-if __name__ == '__main__':
-    x = np.array([0, 2, 4, 6, 8, 10])
-    y = np.sin(x)
 
-    x_new = np.linspace(-1, 11, 150) 
+x = np.array([0, 2, 4, 6, 8, 10])
+y = np.sin(x)
 
-    y_original = [np.sin(val) for val in x_new]
+x_new = np.linspace(-1, 11, 150) 
 
-    y_new = linear_interpolation_1d(x, y, x_new)
-    plt.figure(figsize=(10, 6))  
-    plt.plot(x, y, 'o', label='Исходные точки (x, y)', markersize=8)  
-    plt.plot(x_new, y_new, '-', label='Линейная интерполяция', linewidth=2) #
-    plt.plot(x_new, y_original, '--', label='Исходная функция sin(x)', linewidth=2) 
-    plt.title('1D линейная интерполяция')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+y_original = [np.sin(val) for val in x_new]
+
+y_new = linear_interpolation_1d(x, y, x_new)
+plt.figure(figsize=(10, 6))  
+plt.plot(x, y, 'o', label='Исходные точки (x, y)', markersize=8)  
+plt.plot(x_new, y_new, '-', label='Линейная интерполяция', linewidth=2) #
+plt.plot(x_new, y_original, '--', label='Исходная функция sin(x)', linewidth=2) 
+plt.title('1D линейная интерполяция')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+plt.show()
